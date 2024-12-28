924683a261eecbec48514d6ca043728bf28a0509
Evangelos Angelakis <167527848+EvangelosAngelakis@users.noreply.github.com>
2024-12-28 12:02:29 +0000

    Added button and changes js to work with button

--- Riddle.1.js
+++ Riddle.1.js
@@ -1,2 +1,3 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Riddle No1";
+document.getElementById('dButton').addEventListener('click', function(){
+document.getElementById('i1').src='https://is1-ssl.mzstatic.com/image/thumb/Purple126/v4/f2/33/64/f23364f6-7d6c-e473-5b19-a3b88d0e5a38/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/230x0w.webp';
+});
